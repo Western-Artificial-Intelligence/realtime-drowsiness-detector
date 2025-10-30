7112b80cd17357553af091e18c12a4066c71b016
nthnfng <nthnfng@gmail.com>
2025-10-30 12:53:19 -0400

    Implement Camera class

--- src/wai/camera.py
+++ src/wai/camera.py
@@ -1,9 +1,14 @@
+import cv2
 import numpy as np
 
 class Camera:
-    def __init__(self, index=0, width=640, height=480):
-        pass
-    def read(self) -> tuple(bool, np.ndarray):
-        pass
-    def release(self) -> None:
-        pass
+	def __init__(self, index=0, width=640, height=480):
+		self.cap = cv2.VideoCapture(index)
+		self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+		self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+	def read(self) -> tuple[bool, np.ndarray]:
+		return self.cap.read()
+	
+	def release(self) -> None:
+		self.cap.release()
