11e57dddd69372c6d338b22dea342280f3d63ce9
nthnfng <nthnfng@gmail.com>
2025-10-30 12:43:42 -0400

    Implement Camera class

---
+++ src/wai/camera.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class Camera:
+    def __init__(self, index=0, width=640, height=480):
+        pass
+    def read(self) -> tuple(bool, np.ndarray):
+        pass
+    def release(self) -> None:
+        pass
